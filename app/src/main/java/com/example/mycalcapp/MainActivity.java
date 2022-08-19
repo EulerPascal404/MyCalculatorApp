46d4484c32539e221b30bdfd95040a90f18491c5
ThePuzzlr <90777621+ThePuzzlr@users.noreply.github.com>
2022-08-19 08:19:28 -0500

    Built sum function

--- app/src/main/java/com/example/mycalcapp/MainActivity.java
+++ app/src/main/java/com/example/mycalcapp/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.mycalcapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void findSum(View view) {
+        EditText input_1 = findViewById(R.id.number_1);
+        EditText input_2 = findViewById(R.id.number_2);
+        TextView answer = findViewById(R.id.answer);
+
+        double num1 = Double.parseDouble((input_1.getText().toString()));
+        double num2 = Double.parseDouble((input_2.getText().toString()));
+        double sum = num1 + num2;
+
+        answer.setText("" + sum);
+    }
+
 }
