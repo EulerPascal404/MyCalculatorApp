8a71a74abfb7b9b93c455679192eb0bd3c4c9b06
ThePuzzlr <90777621+ThePuzzlr@users.noreply.github.com>
2022-08-20 18:05:50 -0500

    Implemented 4 operations and clear button

--- app/src/main/java/com/example/mycalcapp/MainActivity.java
+++ app/src/main/java/com/example/mycalcapp/MainActivity.java
@@ -8,7 +8,6 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,12 +18,61 @@ public class MainActivity extends AppCompatActivity {
         EditText input_1 = findViewById(R.id.number_1);
         EditText input_2 = findViewById(R.id.number_2);
         TextView answer = findViewById(R.id.answer);
-
+        if(input_1.getText().toString().isEmpty() || input_2.getText().toString().isEmpty()){
+            answer.setText("Input needed");
+            return;
+        }
         double num1 = Double.parseDouble((input_1.getText().toString()));
         double num2 = Double.parseDouble((input_2.getText().toString()));
-        double sum = num1 + num2;
-
-        answer.setText("" + sum);
+        double result= num1 + num2;
+        answer.setText("" + result);
+    }
+    public void findDiff(View view) {
+        EditText input_1 = findViewById(R.id.number_1);
+        EditText input_2 = findViewById(R.id.number_2);
+        TextView answer = findViewById(R.id.answer);
+        if(input_1.getText().toString().isEmpty() || input_2.getText().toString().isEmpty()){
+            answer.setText("Input needed");
+            return;
+        }
+        double num1 = Double.parseDouble((input_1.getText().toString()));
+        double num2 = Double.parseDouble((input_2.getText().toString()));
+        double result= num1 - num2;
+        answer.setText("" + result);
+    }
+    public void findProduct(View view) {
+        EditText input_1 = findViewById(R.id.number_1);
+        EditText input_2 = findViewById(R.id.number_2);
+        TextView answer = findViewById(R.id.answer);
+        if(input_1.getText().toString().isEmpty() || input_2.getText().toString().isEmpty()){
+            answer.setText("Input needed");
+            return;
+        }
+        double num1 = Double.parseDouble((input_1.getText().toString()));
+        double num2 = Double.parseDouble((input_2.getText().toString()));
+        double result= num1 * num2;
+        answer.setText("" + result);
+    }
+    public void findQuotient(View view) {
+        EditText input_1 = findViewById(R.id.number_1);
+        EditText input_2 = findViewById(R.id.number_2);
+        TextView answer = findViewById(R.id.answer);
+        if(input_1.getText().toString().isEmpty() || input_2.getText().toString().isEmpty()){
+            answer.setText("Input needed");
+            return;
+        }
+        double num1 = Double.parseDouble((input_1.getText().toString()));
+        double num2 = Double.parseDouble((input_2.getText().toString()));
+        double result= num1 / num2;
+        answer.setText("" + result);
+    }
+    public void clearEntries(View view){
+        EditText input_1 = findViewById(R.id.number_1);
+        EditText input_2 = findViewById(R.id.number_2);
+        TextView answer = findViewById(R.id.answer);
+        input_1.setText("");
+        input_2.setText("");
+        answer.setText("");
     }
 
 }
